3ac6273803001c1c432d636fb4f367a0e58c7f2b
rjzg <81981576+rjzg@users.noreply.github.com>
2021-04-14 14:09:52 -0300

    original file

---
+++ potentiometer.ino
@@ -0,0 +1,17 @@
+int analogInPin = A0;
+
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  }
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.println();
+  
+  delay(2);
+  }
